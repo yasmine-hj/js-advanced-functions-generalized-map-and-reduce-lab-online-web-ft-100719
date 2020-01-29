2861939c6e82c5f2e6bd70e7794ba7e1d2e08ef1
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-29 11:35:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(array, callback){
+    let result = []
+    for(let i = 0; i < array.length; i++){
+        result.push(callback(array[i]))
+    }
+    return result
+}
+
+function reduce(array, callback, start){
+    let result = array[0]
+    let i = 1
+    if (!!start){
+        result = start;
+        i--;
+    }
+    for(; i < array.length; i++){
+        result = callback(array[i], result);
+    }
+    return result
+}
